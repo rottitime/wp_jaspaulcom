faf17266fabc92e3798b495cdbd0b8ae00d7d900
Jas <webmaster@jaspaul.com>
2023-04-01 01:30:34 +0100

    css and script files

---
+++ wp-content/themes/jaspaulcom/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function jaspaulcom_register_styles() {
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style( 'jaspaulcom-style', get_template_directory_uri() . '/style.css', array(), $version, 'all' );
+}
+
+
+add_action( 'wp_enqueue_scripts', 'jaspaulcom_register_styles' );
+
+function jaspaulcom_register_scripts() {
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_script( 'jaspaulcom-scripts', get_template_directory_uri() . '/assets/js/main.js', array(), $version, true );
+}
+
+
+add_action( 'wp_enqueue_scripts', 'jaspaulcom_register_scripts' );
