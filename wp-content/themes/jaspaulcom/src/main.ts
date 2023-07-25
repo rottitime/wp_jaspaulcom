e640b7e5a4b78530261f5f4f9d146d7d194918dd
Jas <webmaster@jaspaul.com>
2023-07-25 13:08:03 +0100

    base  styles

--- wp-content/themes/jaspaulcom/src/main.ts
+++ wp-content/themes/jaspaulcom/src/main.ts
@@ -1,25 +1,24 @@
-// eslint-disable-next-line import/no-unresolved
 import './style/vars.css'
 import './style/style.css'
 
 // import typescriptLogo from './typescript.svg'
 
-import { setupCounter } from './counter'
+// import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
